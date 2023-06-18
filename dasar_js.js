7a6f6ac73fee00d5e843321e2dc8a26c0391cb0b
prasetyatf <58251507+prasetyatf@users.noreply.github.com>
2023-06-19 00:53:28 +0700

    Create dasar_js.js

---
+++ dasar_js.js
@@ -0,0 +1,37 @@
+//dasar javascript by skillvul
+
+let namaVaribel = "Ini variabel yang dapat diubah";
+const _namaVar = "Ini variabel yg tidak dapat diubah";
+
+// aturan penulisan var: tidak boleh diawali angka, syntax js, @, !, #
+// boleh diawali _, KayakGini, KayakGitu, kayak_begitu.
+// jangan lupa titik koma
+// tipe data: string, int, float, boolean, null(tidak ada), undefined(variabel kosong), symbol, object
+
+console.log(namaVariabel); //ini untuk menampilkan hasil kodingan ke tool console di browser
+// supaya tidak muncul di muka page
+
+// operator aritmatika. sama kayak python + operator increment(++) & decrement(--)
+/* assignment kyk python 
+let bilangan += 1;
+const angka **= 2; 
+*/
+
+//operator string +, +=
+/* operator boolean sama kyk python
+
+== berarti  sama dengan(cek nilai)
+
+=== berarti  sama dengan(cek nilai & tipe data)
+
+? : ternary
+variabel = (kondisi true) ? nilai1 : nilai2;
+Artinya apabila kondisi true, maka variabel akan diberi nilai1
+Apabila kondisi false, maka variabel akan diberi nilai2
+*/
+
+// operator logika: &&, ||, ! (and, or, not)
+
+// function
+
+
